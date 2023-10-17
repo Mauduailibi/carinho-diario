72f03ef692ffcdffba82870792063b312a94c90b
Mauricio Duailibi Neto <maudneto@gmail.com>
2023-10-17 15:48:29 -0300

    movie recommendation done

--- app/(routes)/diario/components/client.tsx
+++ app/(routes)/diario/components/client.tsx
@@ -15,6 +15,8 @@ import {
 } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import Image from 'next/image';
+import Link from 'next/link';
 
 interface DiarioClientProps {
   profile: {
@@ -43,6 +45,14 @@ interface DiarioClientProps {
     sign: string;
     date: string;
   };
+  movie: {
+    id: number;
+    title: string;
+    overview: string;
+    poster_path: string;
+    vote_average: number;
+    release_date: string;
+  };
 }
 
 export default function DiarioClient({
@@ -50,6 +60,7 @@ export default function DiarioClient({
   motivationalPhrase,
   tarotCard,
   horoscope,
+  movie,
 }: DiarioClientProps) {
   const [hasBirthday, setHasBirthday] = useState(false);
   const [isTarotBlur, setIsTarotBlur] = useState(true);
@@ -59,6 +70,8 @@ export default function DiarioClient({
       setHasBirthday(true);
     }
   }, [profile]);
+
+  const movieDate = new Date(movie.release_date).toLocaleDateString();
 
   return (
     <>
@@ -79,7 +92,7 @@ export default function DiarioClient({
           <CardHeader>
             <CardTitle>Frase do dia</CardTitle>
           </CardHeader>
-          <CardContent className="text-sm">
+          <CardContent className="text-sm text-justify">
             <p>&quot;{motivationalPhrase.content}&quot;</p>
           </CardContent>
           <CardFooter>
@@ -94,7 +107,7 @@ export default function DiarioClient({
             <CardTitle>{tarotCard.name}</CardTitle>
           </CardHeader>
           <CardContent className={cn('', isTarotBlur && 'blur')}>
-            <div className="space-y-2 font-medium text-sm">
+            <div className="space-y-2 font-medium text-sm text-justify">
               <p>{tarotCard.desc}</p>
               <p className="text-muted-foreground">
                 Significado:{' '}
@@ -117,9 +130,37 @@ export default function DiarioClient({
             <CardTitle>Horóscopo</CardTitle>
             <CardDescription>{horoscope.sign}</CardDescription>
           </CardHeader>
-          <CardContent className="text-sm">
+          <CardContent className="text-sm text-justify">
             <p>{horoscope.horoscope}</p>
           </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>{movie.title}</CardTitle>
+            <CardDescription>{movieDate}</CardDescription>
+          </CardHeader>
+          <CardContent className="text-sm text-justify space-y-3">
+            <Image
+              src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+              width={500}
+              height={750}
+              alt={movie.title}
+              className="w-full lg:w-[200px] h-auto rounded-md"
+            />
+            <p>
+              {movie.overview.length === 0 ? 'Sem descrição' : movie.overview}
+            </p>
+          </CardContent>
+          <CardFooter>
+            <Link
+              href={`https://www.themoviedb.org/movie/${movie.id}`}
+              target="_blank"
+              className="w-full"
+            >
+              <Button className="w-full md:w-auto">Assistir</Button>
+            </Link>
+          </CardFooter>
         </Card>
       </div>
     </>
--- app/(routes)/diario/page.tsx
+++ app/(routes)/diario/page.tsx
@@ -11,58 +11,107 @@ export default async function DiarioPage() {
 
   //    Request Tarot
   async function getTodayTarot() {
-    const response = await axios.get(
-      `${process.env.TAROT_API_URL}/cards/random?n=1`,
-    );
+    try {
+      const response = await axios.get(
+        `${process.env.TAROT_API_URL}/cards/random?n=1`,
+      );
 
-    const tarotCard = response.data.cards[0];
-    const tarotCardKeys = Object.keys(tarotCard);
+      const tarotCard = response.data.cards[0];
+      const tarotCardKeys = Object.keys(tarotCard);
 
-    for (const key of tarotCardKeys) {
-      tarotCard[key] = await translate(tarotCard[key], {
-        from: 'en',
-        to: 'pt',
-      });
+      for (const key of tarotCardKeys) {
+        tarotCard[key] = await translate(tarotCard[key], {
+          from: 'en',
+          to: 'pt',
+        });
+      }
+
+      return tarotCard;
+    } catch (error) {
+      return {};
     }
-
-    return tarotCard;
   }
 
   // Request Motivational Phrase
   async function getMotivationalPhrase() {
-    const response = await axios.get(
-      'https://api.quotable.io/random?tags=love|happiness',
-    );
+    try {
+      const response = await axios.get(
+        `${process.env.QUOTE_API_URL}/random?tags=love|happiness`,
+      );
 
-    const phrase = response.data.content;
-    const phraseTranslated = await translate(phrase, { from: 'en', to: 'pt' });
-    return {
-      content: phraseTranslated,
-      author: response.data.author,
-    };
+      const phrase = response.data.content;
+      const phraseTranslated = await translate(phrase, {
+        from: 'en',
+        to: 'pt',
+      });
+
+      return {
+        content: phraseTranslated,
+        author: response.data.author,
+      };
+    } catch (error) {
+      return {
+        content: '',
+        author: '',
+      };
+    }
   }
 
   // Request todays horoscope
   async function getTodayHoroscope() {
-    const sign = await getZodiacSign(
-      profile.birthday.toISOString().split('T')[0],
-    );
+    try {
+      const sign = await getZodiacSign(
+        profile.birthday.toISOString().split('T')[0],
+      );
 
-    const response = await axios.post(`https://newastro.vercel.app/`, {
-      date: new Date().toISOString().split('T')[0],
-      sign,
-      lang: 'pt',
-    });
+      const response = await axios.post(`${process.env.HOROSCOPE_API_URL}`, {
+        date: new Date().toISOString().split('T')[0],
+        sign,
+        lang: 'pt',
+      });
 
-    return {
-      ...response.data,
-      sign: await getPortugueseSign(sign),
-    };
+      return {
+        ...response.data,
+        sign: await getPortugueseSign(sign),
+      };
+    } catch (error) {
+      return {};
+    }
+  }
+
+  // Request movie recommendation
+  async function getMovieRecommendation() {
+    // random int number until 40536
+    const randomPage = Math.floor(Math.random() * 500) + 1;
+    const randomResults = Math.floor(Math.random() * 20);
+
+    try {
+      const response = await axios.get(
+        `${process.env.TMDB_API_URL}/discover/movie`,
+        {
+          params: {
+            include_adult: 'false',
+            include_video: 'false',
+            language: 'pt-BR',
+            page: randomPage,
+          },
+          headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${process.env.TMDB_API_AUTH}`,
+          },
+        },
+      );
+
+      return response.data.results[randomResults];
+    } catch (error) {
+      return {};
+    }
   }
 
   const tarotCard = await getTodayTarot();
   const motivationalPhrase = await getMotivationalPhrase();
   const horoscope = await getTodayHoroscope();
+  const movieRecommendation = await getMovieRecommendation();
 
   return (
     <div className="w-full">
@@ -71,6 +120,7 @@ export default async function DiarioPage() {
         motivationalPhrase={motivationalPhrase}
         tarotCard={tarotCard}
         horoscope={horoscope}
+        movie={movieRecommendation}
       />
     </div>
   );
